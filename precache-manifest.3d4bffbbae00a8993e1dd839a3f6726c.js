ff4d96a00d4367ed4bdcf11742ed1d0cff9366d7
ibulavsky <ibulavsky@gmail.com>
2019-12-25 16:15:10 +0300

    Updates

---
+++ precache-manifest.3d4bffbbae00a8993e1dd839a3f6726c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba9c5748a4ff88e2a1078129fcb097b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "427d477f839a8d195fe9",
+    "url": "/portfolio/static/css/main.c9624754.chunk.css"
+  },
+  {
+    "revision": "beb8ebdbe2362b12621f",
+    "url": "/portfolio/static/js/2.cfa65510.chunk.js"
+  },
+  {
+    "revision": "427d477f839a8d195fe9",
+    "url": "/portfolio/static/js/main.822365fd.chunk.js"
+  },
+  {
+    "revision": "c5306228cc83f6f6271c",
+    "url": "/portfolio/static/js/runtime-main.7676bd13.js"
+  },
+  {
+    "revision": "ae593da5f0660389a5162c348923dd76",
+    "url": "/portfolio/static/media/counterIcon.ae593da5.svg"
+  },
+  {
+    "revision": "f41a2b43aead2f4f8cdcdb0d2afc7eb0",
+    "url": "/portfolio/static/media/cssBlackIcon.f41a2b43.svg"
+  },
+  {
+    "revision": "ba679906e271d70b0c6c0f2460e8d2e5",
+    "url": "/portfolio/static/media/github.ba679906.svg"
+  },
+  {
+    "revision": "18c67c5a47ebd88e91c7249eee0d90d3",
+    "url": "/portfolio/static/media/githubBlackItem.18c67c5a.svg"
+  },
+  {
+    "revision": "5435d7d30d61107e0266556996b0460a",
+    "url": "/portfolio/static/media/htmlBlackIcon.5435d7d3.svg"
+  },
+  {
+    "revision": "6cb9045f8b974c7bb4e120b2767cff94",
+    "url": "/portfolio/static/media/jsBlackItem.6cb9045f.svg"
+  },
+  {
+    "revision": "02ec16b9ae4bdf68167ea981273dcc09",
+    "url": "/portfolio/static/media/linkedin.02ec16b9.svg"
+  },
+  {
+    "revision": "62e1fc1c7ca26024460506b62431e28d",
+    "url": "/portfolio/static/media/myPhotoRick.62e1fc1c.jpg"
+  },
+  {
+    "revision": "a89588fc3296b7ca57cd407c17f65c2a",
+    "url": "/portfolio/static/media/reactBlackIcon.a89588fc.svg"
+  },
+  {
+    "revision": "f09f62e001265a91d70767a7b6e36abf",
+    "url": "/portfolio/static/media/reduxBlackIcon.f09f62e0.svg"
+  },
+  {
+    "revision": "7e7271d23e52df9b527fde819c440e25",
+    "url": "/portfolio/static/media/telegram.7e7271d2.svg"
+  },
+  {
+    "revision": "de87c0ca5d4a29fa7ec64d683b694b3d",
+    "url": "/portfolio/static/media/vk.de87c0ca.svg"
+  },
+  {
+    "revision": "950fa6dc71240918f6af49aea33f1c5a",
+    "url": "/portfolio/static/media/whishesList.950fa6dc.jpg"
+  }
+]);
