636ece732892e2b2695c26a515250352c7524b11
ibulavsky <ibulavsky@gmail.com>
2020-01-28 20:41:05 +0300

    Changed "about me".

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import Fade from 'react-reveal/Fade';
 import styles from './AboutMe.module.css';
 import Title from "../../common/Title";
+import myPhoto from '../../assets/Photo/myPhoto_.jpg'
 
 const AboutMe = () => (
-        <div id='about' className={styles.greeting}>
-            <Fade bottom>
+    <div id='about' className={styles.greeting}>
+        <Fade bottom>
             <Title title={'About me'}/>
-            {/*<img src={''} className={styles.photo}/>*/}
             <div className={styles.description}>
-                <p>My name is Igor. I'm 25 years old.</p>
-                <p>I graduated from Belarussian National Technical University in 2016. I am working as design
-                    engineer.</p>
-                <p>I fond of front-end development. I am attending courses, which called "it-incubator". I know js,
-                    typescript, react, css.</p>
-                <p>Also I like to spend free time with my friends. </p>
+                <img src={myPhoto} className={styles.photo}/>
+                <div>My name is Igor. I'm 25 years old.
+                    I graduated from Belarussian National Technical University in 2016. I am working as design
+                    engineer.
+                    I fond of front-end development. I am attending courses, which called "it-incubator". I know js,
+                    typescript, react, css.
+                    Also I like to spend free time with my friends.
+                </div>
             </div>
-            </Fade>
-        </div>
+        </Fade>
+    </div>
 )
 
 export default AboutMe;
